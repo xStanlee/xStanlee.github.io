f5d6230d6b79924c4b8acfee1890ed930c19584a
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2021-01-29 10:45:29 +0100

    ' --> on listener first param forgot
    What's wrong with me today?!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded, (ev) => {
+window.addEventListener('DOMContentLoaded', (ev) => {
   alert('Press enter to apply correct action on our LED screen.');
 
   const domElem = document.documentElement;
