5a7e23394089d2d55ddc505511192305e019b204
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2021-01-29 10:31:21 +0100

    main js FILE

---
+++ js/app.js
@@ -0,0 +1,14 @@
+doucment.addEventListener('DOMContentLoaded, () => {
+  alert('Press enter to apply correct action on our LED screen.');
+
+  const domElem = document.documentElement;
+  const check = document.querySelector('.side-nav__link > span')
+
+  document.addEventListener('keypress', fullScreenMode);
+
+  
+  function fullScreenMode(e) {
+    if(e.code === 'Enter') { domElem.requestFullscreen ? domElem.requestFullscreen() : alert('Problem with full screen mode please press F11');}
+  }
+
+}
