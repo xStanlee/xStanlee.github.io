f3232d9c8720acd013eb7fdafda8bb45710280d5
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2021-01-29 10:42:44 +0100

    ) --> forgot
    QuickScript

--- js/app.js
+++ js/app.js
@@ -2,13 +2,11 @@ window.addEventListener('DOMContentLoaded, (ev) => {
   alert('Press enter to apply correct action on our LED screen.');
 
   const domElem = document.documentElement;
-  const check = document.querySelector('.side-nav__link > span')
-
   document.addEventListener('keypress', fullScreenMode);
-
-  
+ 
   function fullScreenMode(e) {
-    if(e.code === 'Enter') { domElem.requestFullscreen ? domElem.requestFullscreen() : alert('Problem with full screen mode please press F11');}
+    if(e.code === 'Enter') { 
+      domElem.requestFullscreen ? domElem.requestFullscreen() : alert('Problem with full screen mode please press F11');
+    }
   }
-
-}
+});
